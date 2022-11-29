fb3eb766b32f77171e216ce3fd5e4b59e5b1ded5
Yuma Ono <yuma.ono@rakus-partners.co.jp>
2022-11-29 17:40:33 +0900

    11/29修正

--- src/test/java/com/example/controller/AutoCompleteApiControllerTest.java
+++ src/test/java/com/example/controller/AutoCompleteApiControllerTest.java
@@ -1,54 +1,66 @@
-//package com.example.controller;
-//
-//import static org.hamcrest.CoreMatchers.is;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//
-//import java.awt.PageAttributes.MediaType;
-//import java.util.Arrays;
-//import java.util.List;
-//
-//import org.junit.jupiter.api.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.test.context.junit4.SpringRunner;
-//import org.springframework.test.web.servlet.MockMvc;
-//
-//import com.example.domain.Item;
-//import com.example.service.ItemService;
+package com.example.controller;
 
-//class AutoCompleteApiControllerTest {
-//
-//	@RunWith(SpringRunner.class)
-//	@WebMvcTest(AutoCompleteApiController.class)
-//	public class ListCafeControllerTest {
-//
-//	  @Autowired
-//	  private MockMvc mvc;
-//
-//	  @MockBean
-//	  private ItemService ItemService;
-//
-//	  @Test
-//	  public void test() throws Exception {
-//	    Item item = new Item();
-//	    item.setName("Test Item");
-//
-//	    List<Item> allNames = Arrays.asList(item);
-//
-//	    given(ItemService.findAll())
-//        .willReturn(allNames);
-//
-//	    mvc.perform(get("/searchItem")
-//	        .contentType(MediaType.APPLICATION_JSON))
-//	        .andExpect(status().isOk())
-//	        .andExpect(jsonPath("$", hasSize(1)))
-//	        .andExpect(jsonPath("$[0].name", is(item.getName())));
-//
-//	  }
-//
-//	}
-//}
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.example.domain.Item;
+import com.example.service.ItemService;
+
+@SpringBootTest
+class AutoCompleteApiControllerTest {
+
+	@Autowired
+	private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	  @Test
+	  public void test() throws Exception {
+	    Item item = new Item();
+	    item.setName("Test Item");
+
+	    List<Item> allNames = Arrays.asList(item);
+
+	    MvcResult mvcResult = mockMvc.perform(get("/searchItem")).andReturn();
+	    String result = mvcResult.getResponse().getContentAsString();
+	    System.out.println(result);
+
+
+	  }
+	
+}
