6c53cf120ed1105b4eddc3bd493b67fe4c5635bc
nakamura <nakamura@gmail.com>
2022-11-30 14:50:37 +0900

    11/30 rrr

--- src/test/java/com/example/controller/AdministratorItemControllerTest.java
+++ src/test/java/com/example/controller/AdministratorItemControllerTest.java
@@ -103,7 +103,7 @@ class AdministratorItemControllerTest {
 	        Item item1 = itemList.get(0);
 	        Item item2 = itemList.get(itemList.size()-1);
 	        assertAll(
-	    			() -> assertEquals("カツカレー　", item1.getName()),
+	    			() -> assertEquals("カツカレー", item1.getName()),
 	    			() -> assertEquals("とんかつカレーラーメン", item2.getName())
 	    		);
 	        //18件あり1ページにつき5件表示させる場合→1,2,3、4がpageNumbersに入る
@@ -115,8 +115,76 @@ class AdministratorItemControllerTest {
 	    			() -> assertEquals(4, integer4)
 	    		);
 	   }
-	   
-	   /*No ModelAndViwe found*/
+   	@Test
+   	@DisplayName("空文字検索")
+	    void showList3() throws Exception {
+   		MvcResult mvcResult = mockMvc.perform(get("/showList")
+           	.param("searchName", " "))
+   			.andExpect(view().name("item/item_list_curry"))
+   			.andReturn();
+   		
+   		//スコープデータの取り出し
+	    	ModelAndView mav = mvcResult.getModelAndView();
+	        Page<Item> pageList = (Page<Item>)mav.getModel().get("itemPage");
+	        List<Item> itemList = pageList.getContent();
+	        
+	        //テスト
+	        //最初と最後を表示
+	        Item item1 = itemList.get(0);
+	        Item item2 = itemList.get(itemList.size()-1);
+	        assertAll(
+	    			() -> assertEquals("カツカレー", item1.getName()),
+	    			() -> assertEquals("とんかつカレーラーメン", item2.getName())
+	    	);
+   		
+   }
+   	
+   	@Test
+   	@DisplayName("曖昧検索（検索名(カツカレー)")
+	    void showList4() throws Exception {
+   		MvcResult mvcResult = mockMvc.perform(get("/showList")
+       		.param("searchName", "カツカレー"))
+   			.andExpect(view().name("item/item_list_curry"))
+   			.andReturn();
+   		
+   		//スコープデータの取り出し
+	    	ModelAndView mav = mvcResult.getModelAndView();
+	        Page<Item> pageList = (Page<Item>)mav.getModel().get("itemPage");
+	        List<Item> itemList = pageList.getContent();
+	        
+	        //テスト
+	        //最初と最後を表示
+	        Item item1 = itemList.get(0);
+	        Item item2 = itemList.get(itemList.size()-1);
+	        assertAll(
+	    			() -> assertEquals("カツカレー", item1.getName()),
+	    			() -> assertEquals("ヒレカツカレー", item2.getName())
+	    	);
+   		
+   }
+   	
+   	@Test
+   	@DisplayName("該当検索結果なし")
+	    void showList5() throws Exception {
+   		MvcResult mvcResult = mockMvc.perform(get("/admiShowList")
+       		.param("searchName", "あああ"))
+   			.andExpect(view().name("item/admi_item_list_curry"))
+   			.andReturn();
+   		
+   		//スコープデータの取り出し
+	    	ModelAndView mav = mvcResult.getModelAndView();
+	        Page<Item> pageList = (Page<Item>)mav.getModel().get("itemPage");
+	        List<Item> itemList = pageList.getContent();
+	        
+	        //テスト
+	        //エラーメッセージが表示
+			
+			String message = (String)mav.getModel().get("message");
+			
+			assertEquals("該当する商品がありません", message);
+	    	
+   		
+   }
 	        @Test
 	    	@DisplayName("商品登録画面の表示")
 	    	void toInsertItemTest1() throws Exception {
@@ -139,9 +207,8 @@ class AdministratorItemControllerTest {
 	            .andExpect(MockMvcResultMatchers.model().errorCount(1))
 	            .andExpect(MockMvcResultMatchers.model().attributeHasFieldErrorCode("itemForm", "description", "NotBlank"));*/
 	        	
-	        	mockMvc.perform(get("/toInsertItem")
-	        	.param("name", "ビーフカレー")
-	            .param("description", ""))
+	        	mockMvc.perform(get("/insertItem")
+	    	 	.param("name", " "))
 	        	.andExpect(view().name("item/item_insert"));
 	        }
 	        	
@@ -186,12 +253,12 @@ class AdministratorItemControllerTest {
 	 	  @DisplayName("エラーが存在する場合には、商品編集画面に遷移")
 	 	  void admiUpdate1() throws Exception {
 	 		 mockMvc.perform(get("/admiUpdate")
-	 				.param("id","1")
-	 	      .param("name", " ")
-	 	      .param("description", " ")
-	 	      .param("price_m"," ")
-	 	      .param("price_m",""))
-	 	      .andExpect(view().name("item/item_edit"));
+	 			.param("id","1")
+	 			.param("name", " ")
+	 			.param("description", " ")
+	 			.param("price_m"," ")
+	 			.param("price_m",""))
+	 		 	.andExpect(view().name("item/item_edit"));
 	 		  
 	 	  }
 	 	  
